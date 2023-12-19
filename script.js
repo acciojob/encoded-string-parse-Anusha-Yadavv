4fb3fdf062b80d23c8d1f9c23af4ef22b1354cfa
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-19 18:45:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 const parseCode = (str) => {
   // your code here
 
-	const [firstName, lastName, id] = str.split("000");
-
-  // Create and return the object with firstName, lastName, and id properties
-  return {
-    firstName: firstName,
-    lastName: lastName,
-    id: id
-  };
-	
-	/*let firstName="";
-	let lastName="";
-	let id;
-	let obj={};
 	let arr=str.split("0");
 	let newArr=[];
 	for(let eachOne of arr){
@@ -21,21 +8,19 @@ const parseCode = (str) => {
 			newArr.push(eachOne);
 		}
 	}
-	let m=newArr.length;
-	firstName=newArr[0];
-	lastName=newArr[1];
-	id=newArr[2];
+	let obj={
+		firstName:newArr[0],
+		lastName:newArr[1],
+		id:newArr[2]
+	}
 	
-		obj[firstName]=firstName;
-		obj[lastName]=lastName;
-		obj[id]=id;
 	return obj;
-	*/
+	
 	
 	
 	
 };
 
 // Do not change the code below
-const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+// const str = prompt("Enter str: ");
+// alert(JSON.stringify(parseCode(str)));
